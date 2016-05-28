841637a180199c838345069955884084eceb8bbc
--g <--g>
2016-05-28 11:46:46 -0700

    finish app component

--- app/App.js
+++ app/App.js
@@ -19,19 +19,95 @@ let realm = new Realm({
 //Query Realm for all Categories
 let favs = realm.objects('Categories')
 
-// Add item to Realm
-_addItem () {
-  if (this.state.input === '' ) return
-  realm.write(() => {
-    realm.create('Categories', { name: this.state.input })
-  })
-}
+class App extends Component {
 
-//Delete item from Realm
-_deleteItem () {
-  let itemToDelete =  favs.filtered('name = $0', name)
-  realm.write(() => {
-    realm.delete(itemToDelete)
-  })
-  this.forceUpdate()
+  constructor () {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  _closeModal () {
+    this.props.navigator.pop()
+  }
+
+  _updateInput (input) {
+    this.setState({ input })
+  }
+
+  // Add item to Realm
+  _addItem () {
+    if (this.state.input === '' ) return
+    realm.write(() => {
+      realm.create('Categories', { name: this.state.input })
+    })
+  }
+
+  //Delete item from Realm
+  _deleteItem () {
+    let itemToDelete =  favs.filtered('name = $0', name)
+    realm.write(() => {
+      realm.delete(itemToDelete)
+    })
+    this.forceUpdate()
+  }
+
+  _viewImages (category) {
+    this.props.navigator.push({
+      name: 'ViewImages',
+      passProps: {
+        closeModal: this._closeModal,
+        category
+      }
+    })
+  }
+
+  render () {
+    let favorites = _.map(favs, (f, i) => {
+      return (
+        <View key={i} style={style.favoriteButtonContainer}>
+        <TouchableHighlight
+          onPress={() => this._viewImages(f.name)}
+          underlayColor='transparent'
+          style={style.favorite}>
+          <Text style={style.favoriteText}>{f.name}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => this._deleteItem(f.name)}
+          underlayColor='transparent'
+          style={style.deleteButton}>
+          <Text style={style.deleteText}>&times;</Text>
+        </TouchableHighlight>
+      </View>)
+    })
+    return (
+      <View style={style.container}>
+        <View style={style.headingContainer}>
+          <Text style={style.heading}>
+            Welcome to Realm Imgur Viewer
+          </Text>
+        </View>
+        <ScrollView style={style.mainContainer}>
+          <TextInput
+            value={this.state.input}
+            onChangeText={(text) => this._updateInput(text)}
+            style={style.input}
+            placeholder='What Do You Like?' />
+          <View style={style.buttonContainer}>
+            <TouchableHighlight
+              underlayColor='#3f62aa'
+              style={[ style.button ]}
+              onPress={() => this._addItem()}>
+              <Text style={style.buttonText}>Add Item</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={style.favContainer}>
+            <Text style={style.favorites}>FAVORITES</Text>
+            {favorites}
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
 }
