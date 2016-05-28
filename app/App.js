d6c5e45cf4e5deb74e1d316aa5bfa732bf7486dc
--g <--g>
2016-05-28 11:35:57 -0700

    add and delete items from realm

--- app/App.js
+++ app/App.js
@@ -11,8 +11,27 @@ import React, {
 import Realm from 'realm'
 import _ from 'lodash'
 
+//Create Realm schema
 let realm = new Realm({
   schema: [{name: 'Categories', properties: {name: 'string'}}]
 })
 
+//Query Realm for all Categories
 let favs = realm.objects('Categories')
+
+// Add item to Realm
+_addItem () {
+  if (this.state.input === '' ) return
+  realm.write(() => {
+    realm.create('Categories', { name: this.state.input })
+  })
+}
+
+//Delete item from Realm
+_deleteItem () {
+  let itemToDelete =  favs.filtered('name = $0', name)
+  realm.write(() => {
+    realm.delete(itemToDelete)
+  })
+  this.forceUpdate()
+}
